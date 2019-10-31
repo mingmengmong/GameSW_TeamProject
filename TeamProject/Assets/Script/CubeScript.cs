55d42031ddf3f902eb2e4c18e00d31732a2fb895
Kim Myung Soo <msbmkim@gmail.com>
2019-10-31 17:47:43 +0900

    김명수 : 샘플 코드 커밋

---
+++ TeamProject/Assets/Script/CubeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+    private Vector3 rotate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotate = 60f * Time.deltaTime * Vector3.one;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Rotate(rotate);
+    }
+}
