f3d74145608d21ebc9a688d79f1b8d4c341f63c9
javacom12 <55723151+javacom12@users.noreply.github.com>
2019-11-10 10:47:48 +0900

    버튼이랑 씬전환 구현

---
+++ TeamProject/Assets/Script/Exit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnMouseExit()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
