6ac7f5d1834017b28a7184a33fa4f691b89eea6b
n20-Eray <gameplayern20@gmail.com>
2019-11-10 18:33:18 +0900

    Move Script
    For Object

---
+++ TeamProject/Assets/Script/field/Object_Moving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Moving : MonoBehaviour
+{
+    public Sprite[] move;
+    int animIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animIndex ++;
+        if (animIndex >= move.Length)
+        {
+            animIndex = 0;
+        }
+        GetComponent<SpriteRenderer>().sprite = move[animIndex];
+    }
+}
