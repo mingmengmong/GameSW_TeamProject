b1a256d6b9a5022a3ef686ce7d01df2e64881e03
Kim Myung Soo <msbmkim@gmail.com>
2019-11-14 17:38:46 +0900

    오브젝트(병아리) 이동 로직 변경 	소스및 타이틀 병아리 수정

--- TeamProject/Assets/Script/field/Object_Moving.cs
+++ TeamProject/Assets/Script/field/Object_Moving.cs
@@ -4,23 +4,48 @@ using UnityEngine;
 
 public class Object_Moving : MonoBehaviour
 {
+    // 이미지 스프라이트 리스트
     public Sprite[] move;
-    int animIndex;
+    // 이미지 순서 별 인덱스 리스트 ( 해당 인덱스는 스프라이트 리스트 인덱스를 나타낸다. )
+    public int[] moveOrderSpriteArr;
+    // 이미지 순서 별 시간 ( ms )
+    public int[] moveOrderTimeArr;
+    // 첫 이미지 랜덤 표기 여기
+    public bool isRandom = false;
+
+    private int moveOrderIndex = 0;
+    private float time = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        animIndex = 0;
+        if (isRandom) {
+            moveOrderIndex = Random.Range(0, moveOrderSpriteArr.Length);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        animIndex ++;
-        if (animIndex >= move.Length)
-        {
-            animIndex = 0;
+    void Update() {
+        time += Time.deltaTime*1000;
+        if (moveOrderTimeArr[moveOrderIndex] <= time) {
+            time = 0;
+            moveOrderIndex++;
+            moveOrderIndex %= moveOrderSpriteArr.Length;
+
+            Debug.Log(moveOrderIndex);
+            
+            GetComponent<SpriteRenderer>().sprite = move[moveOrderSpriteArr[moveOrderIndex]];
         }
-        GetComponent<SpriteRenderer>().sprite = move[animIndex];
+        
+        
+        
+        /*
+        moveOrderIndex ++;
+        if (moveOrderIndex >= move.Length)
+        {
+            moveOrderIndex = 0;
+        }
+        GetComponent<SpriteRenderer>().sprite = move[moveOrderIndex];
+        */
     }
 }
